1c8a515c665119c3cadcfd4738d1d7cf745c8d99
rykun <rykunek@hotmail.com>
2018-01-07 23:07:47 +0100

    init

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const mongoose = require('mongoose');
+
+const passport = require('passport');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+require('./routes/testRoutes')(app);
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server listen at ${PORT}`);
+});
