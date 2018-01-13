cd8194790b8b268c507e900d7b02b9c62c7fe563
rykun <rykunek@hotmail.com>
2018-01-13 15:14:45 +0100

    add bootstrap, flat-ui, remove materialize

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,13 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import 'materialize-css/dist/js/materialize.min.js'
+import 'bootstrap/dist/css/bootstrap.css';
+import './lib/flat-ui/dist/css/flat-ui.css';
+import 'jquery/src/jquery.js';
+
+
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
